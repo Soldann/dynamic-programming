3510c9e5e1ac6b9540c95daeba97fa19a765ea0e
clandsmeer <clandsmeer@student.ethz.ch>
2024-12-09 11:50:26 +0100

    Add get_neighbours function: edges for BFS traversal

--- Solver.py
+++ Solver.py
@@ -21,6 +21,41 @@
 import numpy as np
 from utils import *
 
+def get_neighbours() -> np.array:
+    """
+    For the BFS jumpstart: get a np.array that shows what the neighbours 
+    of points are
+    
+    ### Returns
+    - np.array() dtype int
+        - Each of the K rows represents a current postiion x_k. Each row
+          contains 8 elements that are the approximate neighbours: nodes
+          x_k-1 that could have been used to transition to node x_k. When
+          these nodes would be outside the grid, np.nan is inserted.
+    """
+
+    # An array of shape (n*m x 8): each row stands for a drone state, the 
+    # elements of each row are the indices of the 8 drone states that are
+    # neighbours of this drone state. When there are less than 8 neighbours
+    # they are padded with np.nan
+
+    # Create a grid of indices
+    indices = np.arange(Constants.N * Constants.M).reshape(Constants.N, Constants.M)
+    # Create padded array with -1 on the borders
+    padded_indices = np.pad(indices, pad_width=1, mode='constant', constant_values=-1)
+    # Collect the 8 neighbors using slicing
+    neighbours = np.stack([
+        padded_indices[:-2, :-2], padded_indices[:-2, 1:-1], padded_indices[:-2, 2:],
+        padded_indices[1:-1, :-2], padded_indices[1:-1, 2:],
+        padded_indices[2:, :-2],  padded_indices[2:, 1:-1], padded_indices[2:, 2:]
+    ], axis=-1).reshape(-1, 8)
+    # replace -1 by np.nan:
+    neighbours = np.where(neighbours == -1, np.nan, neighbours)
+
+    # The grid positions with stationary drones should not be available neighbours for 
+    # any grid position
+    drone_pos = np.ravel_multi_index(Constants.DRONE_POS.T, (Constants.N, Constants.M))
+
 
 def solution(P, Q, Constants):
     """Computes the optimal cost and the optimal control input for each
@@ -61,3 +96,5 @@ def solution(P, Q, Constants):
 
     return J_opt, u_opt
 
+if __name__ == "__main__":
+    get_neighbours(4,4)
